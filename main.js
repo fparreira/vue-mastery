ef377609b530ca253d1dd6dfde25da1046b1a000
Fernando Parreira <fparreira@gmail.com>
2021-04-19 08:15:02 -0300

    conditionals and list rendering created

--- main.js
+++ main.js
@@ -4,6 +4,20 @@ var app = new Vue({
         product: "socks",
         image: "assets/vmSocks-blue-onWhite.jpg",
         link: "https://www.google.com",
-        inventory: 100
+        inventory: 100,
+        onSale: false,
+        details: ["80% cotton", "20% poliester", "Gender-neutral"],
+        variants:[
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ],
+        sizes: ["P", "M", "G"],
+        cart: 0
     }
 });
